86eb4ae9677836497038eca778628a27e0294039
iamefe <efeativie@gmail.com>
2024-12-11 20:49:44 +0100

    Add auto-password functionality

--- fedora_keyboard_backlight/led_on.sh
+++ fedora_keyboard_backlight/led_on.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# PTYXIS KEYBOARD SHORTCUT COMMAND
+# ptyxis -x "zsh -c '~/fedora_keyboard_backlight/led_on.sh'"
+
+# You might also want to disable the Caps Lock key (physically if need be).
+
+# Source the sudo password from the secure file
+source ~/.fedora_backlight_password.sh
+# Make sure to set strict permission for password script `chmod 600  ~/.fedora_backlight_password.sh`
+
+# Ensure the variable is set
+if [ -z "$SUDO_PASSWORD" ]; then
+  echo "Error: SUDO_PASSWORD is not set. Please check your fedora_backlight_password.sh file."
+  exit 1
+fi
+
+
 # Get the current Scroll Lock LED devices (they might change)
 scroll_led_paths=$(ls /sys/class/leds/ | grep -E 'input[0-9]+::scrolllock')
 
@@ -14,14 +30,18 @@ for scroll_led_path in $scroll_led_paths; do
 
     # Toggle LED on (1) or off (0)
     if [ "$current_brightness" -eq 0 ]; then
-      echo 1 | sudo tee $brightness_path > /dev/null
+      # echo 1 | sudo tee $brightness_path > /dev/null
+      # echo "$SUDO_PASSWORD" | sudo -S tee "$brightness_path" <<< 1 > /dev/null
+      echo "$SUDO_PASSWORD" | sudo -S bash -c "echo 1 > $brightness_path"
       if [ $? -eq 0 ]; then
         echo "Scroll Lock LED turned ON"
       else
         echo "Failed to turn on Scroll Lock LED"
       fi  
     elif [[ "$current_brightness" -eq 1 ]]; then
-       echo 0 | sudo tee $brightness_path > /dev/null
+       # echo 0 | sudo tee $brightness_path > /dev/null
+        # echo "$SUDO_PASSWORD" | sudo -S tee "$brightness_path" <<< 0 > /dev/null
+        echo "$SUDO_PASSWORD" | sudo -S bash -c "echo 0 > $brightness_path"
       if [ $? -eq 0 ]; then
         echo "Scroll Lock LED turned OFF"
       else
